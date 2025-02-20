2b420751576270edd3905d5acf2bea8c022960e0
Jaime Uriel Torres Serrano <juriel@comtor.net>
2025-02-19 22:26:01 -0500

    Event handler and property

--- src/hello-world-element.ts
+++ src/hello-world-element.ts
@@ -1,10 +1,32 @@
-import { LitElement,html } from "lit";
-import { customElement, property } from 'lit/decorators.js';
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
-@customElement('hello-world-element')
-export class HelloWorldElement extends LitElement{
-    protected render() {
-        return html`Hello World Lit Element`;
+@customElement("hello-world-element") // Define the custom element
+export class HelloWorldElement extends LitElement {
+    @property({ type: String }) hello_str = "Hello World";
+    eventList: string[] = [];
+    constructor() {
+        super();
     }
-    
+
+    private clickHandler(event: Event) {
+        console.log("Button Clicked", event);
+        this.eventList.push("Button Clicked " + event.type+" at "+new Date().toLocaleTimeString()); // Add the event to the list
+        this.requestUpdate();
+    };
+
+    protected render() {
+        return html`
+      <h2>${this.hello_str}</h2>
+      <button type="button" @click=${this.clickHandler.bind(this)}>Click Me</button>
+        <ul>
+            ${this.eventList.map((event) => html`<li>${event}</li>`)}   
+        </ul>
+    `;
+    }
+
+    // Disable shadow DOM (optional)
+    protected createRenderRoot() {
+        return this;
+    }
 }
